3a7d0f501fd8b0306e8bc99be0a91c89650be20c
estermaja88@gmail.com <estermaja88@gmail.com>
2015-05-03 20:27:53 +0200

    Database  model improved

--- src/Mpwarfwk/Component/Database/SQL.php
+++ src/Mpwarfwk/Component/Database/SQL.php
@@ -14,74 +14,76 @@ class SQL extends \PDO
 
     private static $db;
 
-    public static function getDB()
+    private $database;
+
+ 
+    public function __construct() 
     {
-        if (!self::$db){
+        $this->database = $this->getDB();
+    }
+
+   
+    private static function getDB()
+    {
+        if (!self::$db)
+        {
              $options = array(
                 \PDO::ATTR_PERSISTENT => true,
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
-            );
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION);
 
-            try {
-               
-
+            try 
+            {
                  self::$db = new \PDO('mysql:host='.self::HOST.';dbname='.self::DB,self::USER, self::PASS);
-
-            } catch (PDOException $e) {
+            } 
+            catch (PDOException $e) {
                 die($e->getMessage());
             }
-
-          
         }
 
         return self::$db;
     }
 
-
-
-
-
-
-    public function selectAll($table, $fetchMode = \PDO::FETCH_ASSOC) {
-        $sql = "SELECT * FROM $table;";
-        $sth = $this->prepare($sql);
-        $sth->execute();
-        return $sth->fetchAll($fetchMode);
+    public function selectAllFromTable($table) 
+    {
+        $statement = $this->database->prepare("SELECT * FROM $table");
+        $statement->execute();
+        return $statement->fetchAll();
     }
-    public function select($sql, $array = array(), $fetchMode = \PDO::FETCH_ASSOC) {
-        $sth = $this->prepare($sql);
-        foreach ($array as $key => $value) {
-            $sth->bindValue("$key", $value);
+
+    public function selectFromTable($query, $data = NULL) 
+    {
+        $statement = $this->database->prepare($query);
+        if($data != NULL){
+            foreach ($data as $key => $actualValue) {
+                $statement->bindValue(":$key", $actualValue);
+            }
         }
-        $sth->execute();
-        return $sth->fetchAll($fetchMode);
+        $statement->execute();
+        return $statement->fetchAll();
     }
-    public function insert($table, $data) {
-        ksort($data);
-        $fieldNames = implode('`, `', array_keys($data));
-        $fieldValues = ':' . implode(', :', array_keys($data));
 
-        $sth = $this->prepare("INSERT INTO $table (`$fieldNames`) VALUES ($fieldValues)");
-        print_r($fieldValues);
-        foreach($data as $key => $value) {
-            $sth->bindValue(":$key", $value);
+    public function insertInTable($query, $data) 
+    {
+        $statement = $this->database->prepare($query);
+        foreach ($data as $key => $actualValue) {
+            $statement->bindValue(":$key", $actualValue);
         }
-        $sth->execute();
+        return $statement->execute();
     }
-    public function update($table, $data, $where) {
-        ksort($data);
-        $fieldDetails = NULL;
-        foreach ($data as $key => $value) {
-            $fieldDetails .= "`$key`=:$key,";
+
+    public function deleteFromTable($table, $id, $value) 
+    {
+        $statement = $this->database->prepare("DELETE FROM $table WHERE $id = '$value' LIMIT 1");
+        return $statement->execute();
+    }
+
+    public function updateTable($query, $data) 
+    {
+        
+        $statement = $this->database->prepare($query);
+        foreach ($data as $key => $actualValue) {
+            $statement->bindValue(":$key", $actualValue);
         }
-        $fieldDetails = rtrim($fieldDetails, ',');
-        $sth = $this->prepare("UPDATE $table SET $fieldDetails WHERE $where");
-        foreach ($data as $key => $value) {
-            $sth->bindValue(":$key", $value);
-        }
-        $sth->execute();
-    }
-    public function delete($table, $where, $limit = 1) {
-        return $this->exec("DELETE FROM $table WHERE $where LIMIT $limit");
-    }
+        return $statement->execute();
+    }  
 }
